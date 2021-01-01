b8b13e89a1dc189c2616f722376248b82fb62c40
littletof <szalay.kristof.sopron@gmail.com>
2021-01-01 20:58:41 +0100

    Add cli.ts

---
+++ cli.ts
@@ -0,0 +1,27 @@
+import {renderMarkdown} from './mod.ts';
+
+if(!Deno.args) {
+    throw Error('Please provide a path or url to a file or a markdown string');
+} else {
+    if(!Deno.args[0] || !Deno.args[1]) {
+        throw Error("Please provide -l, -r and a path, or -s and a markdown string as arguments");
+    }
+
+    let text;
+    switch(Deno.args[0]) {
+        case '-l':
+            text = Deno.readTextFileSync(Deno.args[1]);
+            break;
+        case '-r':
+            const resp = await fetch(Deno.args[1]);
+            text = await resp.text();
+            break;
+        case '-s':
+            text = Deno.args[1];
+            break;
+        default:
+            throw Error('Only -l, -r and -s is allowed');
+    }
+
+    console.log(renderMarkdown(text));
+}
