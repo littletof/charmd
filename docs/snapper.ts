f0919cb6dc4aff6c3d0d2d687e600959603d003b
littletof <szalay.kristof.sopron@gmail.com>
2021-10-21 14:04:07 +0200

    docs: Use snapper for doc image generation

---
+++ docs/snapper.ts
@@ -0,0 +1,16 @@
+import {snap} from 'https://deno.land/x/snapper@v0.0.3/mod.ts';
+import { renderMarkdown } from "../mod.ts";
+
+// Usage:
+// 1. have deno Puppeteer installed
+// 2. deno run -A --unstable .\docs\snapper.ts
+
+let md = Deno.readTextFileSync('docs/docs.md');
+let r = renderMarkdown(md);
+// remove empty lines after header, to save space on image
+r = r.replace(/\n\n/, "\n").replace(/\n\n/, "\n").replace(/\n\n/, "\n").replace(/\n\n/, "\n").replace(/\n\n/, "\n").replace(/\n\n/, "\n").replace(/_{25}/, '');
+
+
+await snap([
+    {content: r, imageSavePath: 'docs/showcase.png', viewport: {width: 675, height: 925}}
+]);
